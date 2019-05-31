12ca4e3d7640e17b2990e275538bd6758611b054
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-05-31 08:04:16 -0400

    Alteração da Imagem

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ document.getElementsByTagName('button')[0].addEventListener('click', () => {
 
         var map = { " ": " ", "!": "!", "@": "@", "#": "#", "$": "$", "%": "%", "¨": "¨", "&": "&", "*": "*", "(": "(", ")": ")", "'": "'", '"': '"', "-": "-", "_": "_", "+": "+", "=": "=", "|": "|", "<": "<", ">": ">", ":": ":", ",": ",", ".": ".", ";": ";", "/": "/", "?": "?", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "0": "0" };
 
-        texto[i].replace(/[\W\[\] ]/g, function (a) { retorno += map[a] || trata })
+        texto[i].replace(/[\W\[\] ]/g, function (a) { retorno += map[a] || a })
 
         const trata = () => {
             if (anchor > chave.length) {
