91ea2390806a3aec6bae88b6b974936a4912acb7
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-06-18 15:51:27 -0400

    Correções e Remoção dos Console Logs

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+
 // Criptografa o texto
 document.getElementsByClassName('button')[0].addEventListener('click', () => {
     let posicoes = retornaPosicoes(removeAcento(capturaInput(1)))
@@ -8,9 +9,8 @@ document.getElementsByClassName('button')[0].addEventListener('click', () => {
     let anchor = 0
 
     for (let i = 0; i < palavra.length; i++) {
-        console.log(palavra[i]);
-        
-        if(palavra[i == " "|| palavra[i] == "'"|| palavra[i] == "!"|| palavra[i] == "@"|| palavra[i] == "#"|| palavra[i] == "$"|| palavra[i] == "%"|| palavra[i] == "¨"|| palavra[i] == "&"|| palavra[i] == "*"|| palavra[i] == "("|| palavra[i] == ")"|| palavra[i] == "-"|| palavra[i] == "_"|| palavra[i] == "="|| palavra[i] == "+"|| palavra[i] == '"'|| palavra[i] == "|"|| palavra[i] == ","|| palavra[i] == "."|| palavra[i] == ";"|| palavra[i] == "<"|| palavra[i] == ">"|| palavra[i] == ":"|| palavra[i] == "?"|| palavra[i] == "/"|| palavra[i] == "ª"|| palavra[i] == "º"|| palavra[i] == "§"]){
+
+        if(palavra[i] == " "|| palavra[i] == "'"|| palavra[i] == "!"|| palavra[i] == "@"|| palavra[i] == "#"|| palavra[i] == "$"|| palavra[i] == "%"|| palavra[i] == "¨"|| palavra[i] == "&"|| palavra[i] == "*"|| palavra[i] == "("|| palavra[i] == ")"|| palavra[i] == "-"|| palavra[i] == "_"|| palavra[i] == "="|| palavra[i] == "+"|| palavra[i] == '"'|| palavra[i] == "|"|| palavra[i] == ","|| palavra[i] == "."|| palavra[i] == ";"|| palavra[i] == "<"|| palavra[i] == ">"|| palavra[i] == ":"|| palavra[i] == "?"|| palavra[i] == "/"|| palavra[i] == "ª"|| palavra[i] == "º"|| palavra[i] == "§"){
             retorno += palavra[i]
         }else{
             if(anchor == posicoes.length){
@@ -25,7 +25,35 @@ document.getElementsByClassName('button')[0].addEventListener('click', () => {
         }
         
     }
-    console.log(retorno);
+    devolveHTML(retorno);
+    
+})
+
+// Descriptografa o texto
+document.getElementsByClassName('button')[1].addEventListener('click', () => {
+    let posicoes = retornaPosicoes(removeAcento(capturaInput(1)))
+    let palavra = removeAcento(capturaInput(0))
+    let retorno = ""
+    let anchor = 0
+
+    for (let i = 0; i < palavra.length; i++) {
+        
+        if(palavra[i] == " "|| palavra[i] == "'"|| palavra[i] == "!"|| palavra[i] == "@"|| palavra[i] == "#"|| palavra[i] == "$"|| palavra[i] == "%"|| palavra[i] == "¨"|| palavra[i] == "&"|| palavra[i] == "*"|| palavra[i] == "("|| palavra[i] == ")"|| palavra[i] == "-"|| palavra[i] == "_"|| palavra[i] == "="|| palavra[i] == "+"|| palavra[i] == '"'|| palavra[i] == "|"|| palavra[i] == ","|| palavra[i] == "."|| palavra[i] == ";"|| palavra[i] == "<"|| palavra[i] == ">"|| palavra[i] == ":"|| palavra[i] == "?"|| palavra[i] == "/"|| palavra[i] == "ª"|| palavra[i] == "º"|| palavra[i] == "§"){
+            retorno += palavra[i]
+        }else{
+            if(anchor == posicoes.length){
+                anchor = 0
+                retorno += alfabeto[ descriptografaCaracter( retornaPosicoes( palavra[i] ), posicoes[anchor] ) ]
+                anchor++
+            }
+            else{
+                retorno += alfabeto[ descriptografaCaracter( retornaPosicoes( palavra[i] ), posicoes[anchor] ) ]
+                anchor++
+            }
+        }
+        
+    }
+    devolveHTML(retorno);
     
 })
 
@@ -59,8 +87,9 @@ const capturaInput = (posicao) => {
 }
 
 const criptografaCaracter = (posicaoLetra, posicaoChave) => {
-        
-    if( ((posicaoLetra[0] + posicaoChave) % Math.pow(alfabeto.length, 2)) > alfabeto.length ){
+    
+
+    if( ((posicaoLetra[0] + posicaoChave) % Math.pow(alfabeto.length, 2)) > alfabeto.length - 1){
         return (posicaoLetra[0] + posicaoChave) % Math.pow(alfabeto.length, 2) - alfabeto.length
     }
     else{
@@ -68,4 +97,26 @@ const criptografaCaracter = (posicaoLetra, posicaoChave) => {
     }
 }
 
+const descriptografaCaracter = (posicaoLetra, posicaoChave) => {
+     
+    if( ((posicaoLetra[0] - posicaoChave) % Math.pow(alfabeto.length, 2)) < 0 ){
+        return (posicaoLetra[0] - posicaoChave) % Math.pow(alfabeto.length, 2) + alfabeto.length
+    }
+    else{
+        return (posicaoLetra[0] - posicaoChave) % Math.pow(alfabeto.length, 2)
+    }   
+}
 
+const devolveHTML = (retorno) => {
+    if(retorno.length < 40){
+        document.getElementById("retorno").innerHTML = retorno
+    }
+    else{
+        document.getElementById("retorno-container").innerHTML = retorno
+        document.getElementsByClassName("container-return")[0].style.display = "block"
+    }
+}
+
+document.getElementsByClassName('button-close')[0].addEventListener('click', () =>{
+    document.getElementsByClassName("container-return")[0].style.display = "none"
+})
